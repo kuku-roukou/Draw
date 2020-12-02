b9bdc51f57fd1d9dbabb8f897776cb7773edd60c
Alena <alena.ryazanova@mail.ru>
2020-12-02 19:37:57 +0300

    123

---
+++ DrawProgram/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace DrawProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonBrush = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.LabelToolbar = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // ButtonBrush
+            // 
+            this.ButtonBrush.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ButtonBrush.Location = new System.Drawing.Point(20, 59);
+            this.ButtonBrush.Name = "ButtonBrush";
+            this.ButtonBrush.Size = new System.Drawing.Size(226, 33);
+            this.ButtonBrush.TabIndex = 1;
+            this.ButtonBrush.Text = "Кисть";
+            this.ButtonBrush.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Location = new System.Drawing.Point(20, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Линия по двум точкам";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button3.Location = new System.Drawing.Point(20, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Линия по N точкам";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button4.Location = new System.Drawing.Point(20, 212);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(226, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Квадрат";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button5.Location = new System.Drawing.Point(20, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(226, 29);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Окружность";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button6.Location = new System.Drawing.Point(20, 307);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(226, 29);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Эллипс";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button7.Location = new System.Drawing.Point(20, 354);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(226, 32);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Треугольник";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button8.Location = new System.Drawing.Point(20, 404);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(226, 64);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Прямоугольный треугольник по двум точкам";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // LabelToolbar
+            // 
+            this.LabelToolbar.AutoSize = true;
+            this.LabelToolbar.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelToolbar.Location = new System.Drawing.Point(12, 20);
+            this.LabelToolbar.Name = "LabelToolbar";
+            this.LabelToolbar.Size = new System.Drawing.Size(234, 36);
+            this.LabelToolbar.TabIndex = 9;
+            this.LabelToolbar.Text = "Панель инструментов";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(345, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1092, 664);
+            this.panel1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1449, 725);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.LabelToolbar);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ButtonBrush);
+            this.Name = "Form1";
+            this.Text = "DrawForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ButtonBrush;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label LabelToolbar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
